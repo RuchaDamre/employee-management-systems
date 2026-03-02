92fc310857f158877ae751a2ed74865f7422fba4
RuchaDamre <ruchadamre2000@gmail.com>
2026-03-02 22:30:22 +0530

    Fix mobile UI

--- Frontend/app/Components/DashboardCompo/MobileNavbar.tsx
+++ Frontend/app/Components/DashboardCompo/MobileNavbar.tsx
@@ -15,8 +15,8 @@ function MobileNavbar({ open, setOpen }: Props) {
     return (
         <>
             <div onClick={() => setOpen(false)} className={`fixed inset-0 bg-black/40 z-40 transition-opacity duration-300 ${open ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} />
-            <nav className={`md:hidden bg-white text-blue-950 w-80 h-screen fixed pt-10 left-0 bottom-0 mr-8 z-50 transform transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : '-translate-x-full'}`}>
-                <button className='fixed top-2 right-3 text-[30px]' onClick={() => setOpen(false)}><IoIosClose /></button>
+            <nav className={`md:hidden bg-white text-blue-950 w-80 h-screen fixed pt-80 left-0 bottom-0 mr-8 z-50 transform transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : '-translate-x-full'}`}>
+                <button className='fixed top-8 right-3 text-[30px]' onClick={() => setOpen(false)}><IoIosClose /></button>
                 <div className="flex flex-col pt-2 px-5 cursor-pointer mt-15">
                     <Link href={'/dashboard'} className={`${pathname === '/dashboard' ? "bg-blue-200" : ""} py-2 rounded-md flex gap-4 w-full pl-20`} onClick={() => setOpen(false)}><FaTachometerAlt /><span>Dashboard</span></Link>
                     <Link href={'/dashboard/employee'} className={`${pathname === '/dashboard/employee' || pathname === '/dashboard/employee/add-employee' || pathname === `/dashboard/employee/${pathID}` || pathname === `/dashboard/employee/edit-employee/${pathID}` ? "bg-blue-200" : ""} py-2 rounded-md flex gap-4 w-full pl-20`} onClick={() => setOpen(false)}><FaUsers /><span>Employees</span></Link>
--- Frontend/app/dashboard/employee/add-employee/page.tsx
+++ Frontend/app/dashboard/employee/add-employee/page.tsx
@@ -48,23 +48,23 @@ function Add() {
             <form className="flex flex-wrap justify-between" onSubmit={handleSubmit}>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="name" className="md:text-md text-sm">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter Username" className="md:w-sm w-96 rounded-md border-2 border-gray-300 md:p-2 p-c mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="text" name="name" id="name" placeholder="Enter Username" className="md:w-sm w-64 rounded-md border-2 border-gray-300 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="email" className="md:text-md text-sm">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter Email" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="email" name="email" id="email" placeholder="Enter Email" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="empid" className="md:text-md text-sm">Employee ID</label>
-                    <input type="text" name="empid" id="empid" placeholder="Enter Employee ID" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="text" name="empid" id="empid" placeholder="Enter Employee ID" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="dob" className="md:text-md text-sm">Date of Birth</label>
-                    <input type="date" id="dob" name="dob" placeholder="Enter Date of Birth" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="date" id="dob" name="dob" placeholder="Enter Date of Birth" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="gender" className="md:text-md text-sm">Gender</label>
-                    <select id="gender" name="gender" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setFormData((prevData) => ({ ...prevData, gender: e.target.value }))}>
+                    <select id="gender" name="gender" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setFormData((prevData) => ({ ...prevData, gender: e.target.value }))}>
                         <option value=''>Select Gender</option>
                         <option value='Male'>Male</option>
                         <option value='Female'>Female</option>
@@ -72,15 +72,15 @@ function Add() {
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="role" className="md:text-md text-sm">Designation</label>
-                    <input type="text" id="role" name="role" placeholder="Designation" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="text" id="role" name="role" placeholder="Designation" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="salary" className="md:text-md text-sm">Salary</label>
-                    <input type="number" id="salary" name="salary" placeholder="Salary" className="md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="number" id="salary" name="salary" placeholder="Salary" className="md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <div className="flex flex-col mb-3">
                     <label htmlFor="img" className="md:text-md text-sm">Upload Image</label>
-                    <input type="file" id="img" name="img" className="file:bg-gray-200 file:cursor-pointer file:border md:file:p-1 file:p-0.5 file:text-[10px] md:file:text-xs md:w-sm w-96 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
+                    <input type="file" id="img" name="img" className="file:bg-gray-200 file:cursor-pointer file:border md:file:p-1 file:p-0.5 file:text-[10px] md:file:text-xs md:w-sm w-64 border-gray-300 rounded-md border-2 md:p-2 p-0.5 mb-2.5 md:text-md text-sm" required onChange={handleChange} />
                 </div>
                 <button className="text-blue-950 w-full rounded-md font-bold bg-blue-200 md:py-2 py-1.5 my-5 cursor-pointer md:text-md text-xs md:text-sm">Add Employee</button>
             </form>
